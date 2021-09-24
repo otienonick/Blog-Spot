39310d16ab2ec70e84f949bf0a71a7b353f25180
otienonick <nick.odongi@student.moringaschool.com>
2021-09-24 11:54:35 +0300

    added likes button

--- Documents/core-projects/Python/Posts_Flask/app/models.py
+++ Documents/core-projects/Python/Posts_Flask/app/models.py
@@ -10,6 +10,10 @@ class User(db.Model,UserMixin):
     password =  db.Column(db.String(150))
     date_created = db.Column(db.DateTime(timezone =True),default = func.now())
     posts = db.relationship('Post',backref = 'user',passive_deletes = True)
+    comments = db.relationship('Comment', backref='user', passive_deletes=True)
+    likes = db.relationship('Like', backref='user', passive_deletes=True)
+
+
 
 
 class Post(db.Model):
@@ -19,6 +23,8 @@ class Post(db.Model):
     date_created = db.Column(db.DateTime(timezone =True),default = func.now())
     author = db.Column(db.Integer,db.ForeignKey('user.id',ondelete = 'CASCADE'),nullable = False)
     comments = db.relationship('Comment', backref='post', passive_deletes=True)
+    likes = db.relationship('Like', backref='post', passive_deletes=True)
+
 
 
 class Comment(db.Model):
@@ -28,6 +34,15 @@ class Comment(db.Model):
     date_created = db.Column(db.DateTime(timezone =True),default = func.now())
     author = db.Column(db.Integer,db.ForeignKey('user.id',ondelete = 'CASCADE'),nullable = False)
     post_id = db.Column(db.Integer,db.ForeignKey('post.id',ondelete = 'CASCADE'),nullable = False)
+
+class Like(db.Model):
+    __tablename__ = 'likes'
+
+    id = db.Column(db.Integer,primary_key = True)
+    author = db.Column(db.Integer,db.ForeignKey('user.id',ondelete = 'CASCADE'),nullable = False)
+    post_id = db.Column(db.Integer,db.ForeignKey('post.id',ondelete = 'CASCADE'),nullable = False)
+
+
 
 
 
--- Documents/core-projects/Python/Posts_Flask/app/views.py
+++ Documents/core-projects/Python/Posts_Flask/app/views.py
@@ -1,7 +1,7 @@
 from operator import pos
 from flask import Blueprint,render_template,request,flash,redirect,url_for
 from flask_login import login_required,current_user
-from .models import  Post,User,Comment
+from .models import  Post,User,Comment,Like
 from . import db
 
 
@@ -92,3 +92,19 @@ def  delete_comment(comment_id):
 
         return redirect(url_for('views.index'))
 
+@views.route('/like-post/<post_id>',methods = ['GET'])
+@login_required
+def like(post_id):
+    post =Post.query.filter_by(id = post_id)
+    like = Like.query.filter_by(author = current_user.id,post_id = post_id).first()
+    if not post:
+        flash('post does not exist',category='error')
+    elif like:
+        db.session.delete(like)   
+        db.session.commit() 
+    else:
+        like = Like(author = current_user.id,post_id = post_id)
+        db.session.add(like)
+        db.session.commit()  
+
+    return redirect(url_for('views.index'))          
